5ecd201c24aeed6df609ed3b8a4990a6b32ade1e
C.N <cn.mbhalati@gmail.com>
2023-03-27 13:06:52 +0200

    commit

--- src/components/Days.tsx
+++ src/components/Days.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/Calendar.module.scss'
 import { format, isSameDay, isSameMonth, parseISO } from 'date-fns'
 import { type Event, type Day } from '~/types/types';
 
-const { active, today, disabled } = styles
+const { active, today, disabled, event } = styles
 
 const Days = ({ isDayActive, isDayToday, isDayInCurrentMonth, day, setSelectedDay, setCurrentMonth, firstDayCurrentMonth, events }: Day) => {
   const SelectingDay = (day: Date) => {
@@ -12,34 +12,35 @@ const Days = ({ isDayActive, isDayToday, isDayInCurrentMonth, day, setSelectedDa
     setSelectedDay(day)
   }
 
-    return (
-        <div
-          className={
+  return (
+      <div
+        className={
+          `
+            ${styles.day} 
+            ${isDayActive ? active : ''} 
+            ${isDayToday ? today : ''} 
+            ${isDayInCurrentMonth ? '' : disabled}
+            ${events && events.some((event: Event) => isSameDay(parseISO(event.startDatetime), day)) ? event : ''}
             `
-              ${styles.day} 
-              ${isDayActive ? active : ''} 
-              ${isDayToday ? today : ''} 
-              ${isDayInCurrentMonth ? '' : disabled}
-            `
-          }
-          onClick={() => SelectingDay(day)}
-        >
-          <div className={styles.topDay}>
-            <span>{format(day, 'd')}</span>
-          </div>
-          <div className={styles.midDay}>
-            {
-              events?.filter((event: Event) => isSameDay(parseISO(event.startDatetime), day)).map((event: Event, index: number) => {
-                return (
-                    <h4 className={styles.nameOfEvent} key={index}>{event.name}</h4>
-                  );
-                })
-              }
-          </div>
-          <div className={styles.bottomDay}>
+        }
+        onClick={() => SelectingDay(day)}
+      >
+        <div className={styles.topDay}>
+          <span>{format(day, 'd')}</span>
         </div>
+        <div className={`${styles.midDay} ${styles.smallScreen}`}>
+          {
+            events?.filter((event: Event) => isSameDay(parseISO(event.startDatetime), day)).map((event: Event, index: number) => {
+              return (
+                  <h4 className={styles.nameOfEvent} key={index}>{event.name}</h4>
+                );
+              })
+            }
         </div>
-      )
+        <div className={styles.bottomDay}>
+      </div>
+      </div>
+    )
 }
 
 export default Days
